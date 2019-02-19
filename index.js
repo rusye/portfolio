a33212835fdb26013cdd70a1d19fd6a099080a24
Russ yev <ruslanyev@gmail.com>
2019-02-18 18:36:26 -0800

    cleaned up code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function dropDownMenu() {
-  let x = document.getElementById("dropDownMenu");
-  if (x.className.indexOf("show") == -1) {
-    x.className += " show";
+  let x = document.getElementById('dropDownMenu');
+  if (x.className.indexOf('show') == -1) {
+    x.className += ' show';
   } else { 
-    x.className = x.className.replace("show", "");
+    x.className = x.className.replace('show', '');
   }
 }
